687b7761e3552d69976e4119e49f1ae12043065e
Jonatan Vahlberg <jonatan.ramses.vahlberg@gmail.com>
2019-04-30 21:26:12 +0200

    initial feature commit

--- app/src/main/java/com/jonatan_vahlberg/eva/MainActivity.java
+++ app/src/main/java/com/jonatan_vahlberg/eva/MainActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.UUID;
 
@@ -15,12 +21,15 @@ public class MainActivity extends AppCompatActivity {
 
     private Realm realm;
     private RealmResults<Message> messages;
+    private RecyclerView mRecycle;
+    private EditText mEdit;
+    private ImageButton mImageBtn;
 
     private RealmChangeListener realmChangeListener = new RealmChangeListener() {
         @Override
         public void onChange(Object o) {
-            RecyclerView rV = findViewById(R.id.recyclerview);
-            rV.getAdapter().notifyDataSetChanged();
+
+            mRecycle.getAdapter().notifyDataSetChanged();
         }
     };
 
@@ -29,24 +38,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        realm = Realm.getDefaultInstance();
-        messages = realm.where(Message.class).findAll();
+        initRealm();
+        initLayout();
 
-        initRecyclerView();
 
 
     }
 
-    private void initRecyclerView(){
 
-        RecyclerView rV = findViewById(R.id.recyclerview);
+
+    private void initRealm(){
+        realm = Realm.getDefaultInstance();
+        messages = realm.where(Message.class).findAll();
+        realm.addChangeListener(realmChangeListener);
+    }
+
+    private void initLayout(){
+        mEdit = findViewById(R.id.edit_text_send);
+        mEdit.setOnEditorActionListener(editorActionListener);
+        mImageBtn = findViewById(R.id.microphone_btn);
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        mRecycle =findViewById(R.id.recyclerview);
 
         //Create New Adapter Based on RecyclerViewAdapter
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(this,this.messages);
         //Set Adapter
-        rV.setAdapter(adapter);
+        mRecycle.setAdapter(adapter);
 
-        //Set layouManager
-        rV.setLayoutManager(new LinearLayoutManager(this));
+        //Set layoutManager
+        mRecycle.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private TextView.OnEditorActionListener editorActionListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            switch (actionId){
+                case EditorInfo.IME_ACTION_DONE:
+                    if(v.getText().toString().equals("")){
+                        return true;
+                    }
+                    else{
+                        sendmessage(v.getText().toString());
+                        v.setText("");
+                        return false;
+                    }
+                default:
+                    return true;
+            }
+        }
+    };
+
+    private void sendmessage(String message) {
+        realm.beginTransaction();
+        Message newMessage = realm.createObject(Message.class,UUID.randomUUID().getMostSignificantBits());
+        newMessage.setSender("Jonatan");
+        newMessage.setMessage(message);
+        realm.commitTransaction();
+
+        realm.beginTransaction();
+        Message newerMessage = realm.createObject(Message.class,UUID.randomUUID().getMostSignificantBits());
+        newerMessage.setSender("EVA");
+        newerMessage.setMessage("Yo My Boy");
+        realm.commitTransaction();
+
     }
 }
